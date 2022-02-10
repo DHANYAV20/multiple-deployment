69467a016777f5bed0d88445c382bc311c8cb5a4
DHANYAV20 <dhanya1509@gmail.com>
2022-02-10 18:40:55 +0530

    first commit

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,19 +1,19 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+import aws_cdk.aws_s3 as _s3
+from aws_cdk.core import Stack
 from constructs import Construct
+import aws_cdk.core as core
 
-class MyFirstCdkProjectStack(Stack):
+class MyArtifactBucketStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str,is_prod=False, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyFirstCdkProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        if is_prod:
+            artifactBucket = _s3.Bucket(self,
+                                        "myProdArtifactBucketId",
+                                        versioned=True,
+                                        encryption=_s3.BucketEncryption.S3_MANAGED,
+                                        removal_policy=core.RemovalPolicy.RETAIN)
+        else:
+            artifactBucket=_s3.Bucket(self,
+                                        "myDevArtifactBucketId")
